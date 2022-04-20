8447bac937ad88db6aaecab4ae1dc7aea583cc26
tham-f <ft12322@hotmail.com>
2022-04-20 15:55:33 -0400

    Added variable validation to account info change forms

--- viewpatient.php
+++ viewpatient.php
@@ -21,6 +21,7 @@ $email = $phonenum = $address = $postalcode = "";
 $biography = "Biography here...";
 $account_created = "";
 $id = "";
+$plaintext = "-plaintext";
 
 if (is_numeric(trim($_GET['PatientID']))) {
 	$id = trim($_GET['PatientID']);
@@ -37,6 +38,7 @@ if ($stmt = $pdo->prepare($sql)) {
 		$patientinfo = $stmt->fetch();
 		// Checks if query successfully returned an existing patient
 		if ($patientinfo != "") {
+			$id = $patientinfo['id'];
 			$fname = $patientinfo['fname'];
 			$lname = $patientinfo['lname'];
 			$healthnum = $patientinfo['username'];
@@ -62,7 +64,8 @@ if ($stmt = $pdo->prepare($sql)) {
 if (isset($_POST['delete-patient'])) {
 	// Write delete query from users and jvp tables
 	$sql = "DELETE FROM users WHERE id = :id;
-					DELETE FROM jvp WHERE id = :id";
+					DELETE FROM jvp WHERE id = :id;
+					DELETE FROM medicalhistory WHERE id = :id;";
 
 	if ($stmt = $pdo->prepare($sql)) {
 		$stmt->bindParam(':id', $id, PDO::PARAM_INT);
@@ -75,6 +78,157 @@ if (isset($_POST['delete-patient'])) {
 							}, 1000);
 						</script>';
 		}
+	}
+} else if (isset($_POST["name"])) {
+	// Validate first and last names
+	if (empty(trim($_POST["fname"]))) {
+		$fname_err = "Please enter your first name.";
+		$fname_valid = $inputinvalid;
+	} elseif (!preg_match('/^[a-zA-Z ]+$/', trim($_POST["fname"]))) {
+		$fname_err = "First name can only contain letters.";
+		$fname_valid = $inputinvalid;
+	} else {
+		$fname = trim($_POST["fname"]);
+		$fname_err = "";
+		$fname_valid = $inputvalid;
+	}
+
+	if (empty(trim($_POST["lname"]))) {
+		$lname_err = "Please enter your last name.";
+		$lname_valid = $inputinvalid;
+	} elseif (!preg_match('/^[a-zA-Z ]+$/', trim($_POST["lname"]))) {
+		$lname_err = "Last name can only contain letters.";
+		$lname_valid = $inputinvalid;
+	} else {
+		$lname = trim($_POST["lname"]);
+		$lname_err = "";
+		$lname_valid = $inputvalid;
+	}
+
+	// Validate email address
+	if (empty(trim($_POST["email"]))) {
+		$email_err = "Please enter an email address.";
+		$email_valid = $inputinvalid;
+	} elseif (!filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)) {
+		$email_err = "Email is invalid. Please enter a valid email address.";
+		$email_valid = $inputinvalid;
+	} else {
+		$email = trim($_POST["email"]);
+		$email_err = "";
+		$email_valid = $inputvalid;
+	}
+
+	// Vaalidate phone number
+	if (empty(trim($_POST["phonenum"]))) {
+		$phonenum_err = "Please enter a phone number.";
+		$phonenum_valid = $inputinvalid;
+	} elseif (!preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", trim($_POST["phonenum"]))) {
+		$phonenum_err = "Please enter a valid phone number.";
+		$phonenum_valid = $inputinvalid;
+	} else {
+		$phonenum = trim($_POST["phonenum"]);
+		$phonenum_err = "";
+		$phonenum_valid = $inputvalid;
+	}
+
+	// Validate address
+	if (empty(trim($_POST["address"]))) {
+		$address_err = "Please enter an address.";
+		$address_valid = $inputinvalid;
+	} else {
+		$address = trim($_POST["address"]);
+		$address_err = "";
+		$address_valid = $inputvalid;
+	}
+
+	// Validate postal code
+	if (empty(trim($_POST["postalcode"]))) {
+		$postalcode_err = "Please enter a postal code.";
+		$postalcode_valid = $inputinvalid;
+	} else {
+		$postalcode = trim($_POST["postalcode"]);
+		$postalcode_err = "";
+		$postalcode_valid = $inputvalid;
+	}
+
+	// Validate healthnum
+	if (empty(trim($_POST["healthnum"]))) {
+		$healthnum_err = "Please enter a healthcard number.";
+		$healthnum_valid = $inputinvalid;
+	} elseif (!preg_match('/^[a-zA-Z0-9-]+$/', trim($_POST["healthnum"]))) {
+		$healthnum_err = "Healthcard number can only contain numbers and hyphens.";
+		$healthnum_valid = $inputinvalid;
+	} elseif (!is_numeric(str_replace("-", "", $_POST["healthnum"])) || strlen(str_replace("-", "", $_POST["healthnum"])) != 10) {
+		$healthnum_err = "Invalid healthcard number.";
+		$healthnum_valid = $inputinvalid;
+	} else {
+
+		// Prepare a select statement
+		$sql = "SELECT id FROM users WHERE username = :healthnum AND id NOT IN ( :id )";
+
+		if ($stmt = $pdo->prepare($sql)) {
+			// Bind variables to the prepared statement as parameters
+			$stmt->bindParam(":healthnum", $param_healthnum, PDO::PARAM_STR);
+			$stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
+			// Set parameters
+			$param_healthnum = trim($_POST["healthnum"]);
+
+			// Attempt to execute the prepared statement
+			if ($stmt->execute()) {
+				if ($stmt->rowCount() > 0) {
+					$healthnum_err = "An existing account already has this healthcard number.";
+					$healthnum_valid = $inputinvalid;
+				} else {
+					$healthnum = trim($_POST["healthnum"]);
+					$healthnum_err = "";
+					$healthnum_valid = $inputvalid;
+				}
+			} else {
+				echo $alert;
+			}
+			// Close statement
+			unset($stmt);
+		}
+	}
+
+	// Save user inputs to variables
+	$address = trim($_POST["address"]);
+	$biography = $_POST["biography"];
+
+	// * Write update query for user, jvp, and medicalhistory tables
+	$sql = "UPDATE users
+					SET fname = :fname, lname = :lname, username = :healthnum, email = :email, phonenumber = :phonenum, address = :address, postalcode = :postalcode, biography = :biography
+					WHERE id = :id;
+					UPDATE jvp
+					SET healthnum = :healthnum, fname = :fname, lname = :lname
+					WHERE id = :id;
+					UPDATE medicalhistory
+					SET healthnum = :healthnum, fname = :fname, lname = :lname
+					WHERE id = :id;";
+
+	// * Prepare statement
+	if ($stmt = $pdo->prepare($sql)) {
+		// * Bind parameters to variables
+		$stmt->bindParam(':id', $param_id, PDO::PARAM_INT);
+		$stmt->bindParam(':fname', $param_fname, PDO::PARAM_STR);
+		$stmt->bindParam(':lname', $param_lname, PDO::PARAM_STR);
+		$stmt->bindParam(':healthnum', $param_healthnum, PDO::PARAM_STR);
+		$stmt->bindParam(':email', $param_email, PDO::PARAM_STR);
+		$stmt->bindParam(':phonenum', $param_phonenum, PDO::PARAM_STR);
+		$stmt->bindParam(':address', $param_address, PDO::PARAM_STR);
+		$stmt->bindParam(':postalcode', $param_postalcode, PDO::PARAM_STR);
+		$stmt->bindParam(':biography', $param_biography, PDO::PARAM_STR);
+
+		$param_id = $id;
+		$param_fname = $fname;
+		$param_lname = $lname;
+		$param_healthnum = $healthnum;
+		$param_email = $email;
+		$param_phonenum = $phonenum;
+		$param_address = $address;
+		$param_postalcode = $postalcode;
+		$param_biography = $biography;
 	}
 }
 ?>
@@ -100,6 +254,7 @@ if (isset($_POST['delete-patient'])) {
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 	<script type="text/javascript" src="animations.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://kit.fontawesome.com/e5e6e4ad79.js" crossorigin="anonymous"></script>
 	<style>
 		html,
@@ -118,6 +273,114 @@ if (isset($_POST['delete-patient'])) {
 </head>
 
 <body>
+	<script>
+		// JQuery function to autoformat healthcard number, phone number, and postal code
+		(function($, undefined) {
+			"use strict";
+
+			// When ready.
+			$(function() {
+
+				var $form = $("#patient-info");
+				var $input1 = $form.find("#healthnum");
+				var $input2 = $form.find("#phonenum");
+				var $input3 = $form.find("#postalcode");
+
+				$input1.on("keyup", function(event) {
+
+					// When user select text in the document, also abort.
+					var selection = window.getSelection().toString();
+					if (selection !== '') {
+						return;
+					}
+
+					// When the arrow keys are pressed, abort.
+					if ($.inArray(event.keyCode, [38, 40, 37, 39]) !== -1) {
+						return;
+					}
+
+					var $this = $(this);
+					var input = $this.val();
+					input = input.replace(/[\W\D\s\._\-]+/g, '');
+
+					var split = 4;
+					var chunk = [];
+
+					for (var i = 0, len = input.length; i < len; i += split) {
+						split = (i >= 4 && i <= 16) ? 3 : 4;
+						chunk.push(input.substr(i, split));
+					}
+
+					$this.val(function() {
+						return chunk.join("-").toUpperCase();
+					});
+
+				});
+
+				$input2.on("keyup", function(event) {
+
+					// When user select text in the document, also abort.
+					var selection = window.getSelection().toString();
+					if (selection !== '') {
+						return;
+					}
+
+					// When the arrow keys are pressed, abort.
+					if ($.inArray(event.keyCode, [38, 40, 37, 39]) !== -1) {
+						return;
+					}
+
+					var $this = $(this);
+					var input = $this.val();
+					input = input.replace(/[\W\D\s\._\-]+/g, '');
+
+					var split = 4;
+					var chunk = [];
+
+					for (var i = 0, len = input.length; i < len; i += split) {
+						split = (i >= 4 && i <= 16) ? 4 : 3;
+						chunk.push(input.substr(i, split));
+					}
+
+					$this.val(function() {
+						return chunk.join("-").toUpperCase();
+					});
+
+				});
+
+				$input3.on("keyup", function(event) {
+
+					// When user select text in the document, also abort.
+					var selection = window.getSelection().toString();
+					if (selection !== '') {
+						return;
+					}
+
+					// When the arrow keys are pressed, abort.
+					if ($.inArray(event.keyCode, [38, 40, 37, 39]) !== -1) {
+						return;
+					}
+
+					var $this = $(this);
+					var input = $this.val();
+					input = input.replace(/[\W\s\._\-]+/g, '');
+
+					var split = 4;
+					var chunk = [];
+
+					for (var i = 0, len = input.length; i < len; i += split) {
+						split = (i >= 4 && i <= 16) ? 4 : 3;
+						chunk.push(input.substr(i, split));
+					}
+
+					$this.val(function() {
+						return chunk.join(" ").toUpperCase();
+					});
+
+				});
+			});
+		})(jQuery);
+	</script>
 	<!-- Delete patient confirmation modal -->
 	<div class="modal" id="delete-patient">
 		<div class="modal-dialog modal-dialog-centered">
@@ -132,7 +395,6 @@ if (isset($_POST['delete-patient'])) {
 				<!-- Modal body -->
 				<div class="modal-body">
 					<form method="post" class="form-horizontal">
-
 						<button type="submit" name="delete-patient" class="btn btn-danger container-fluid">Yes, I'm sure</button>
 					</form>
 				</div>
@@ -192,48 +454,81 @@ if (isset($_POST['delete-patient'])) {
 				<!-- Picture of patient -->
 				<div class="col-sm container p-3 text-center">
 					<img src='images/default.jpg' alt='<?= $fname . " " . $lname; ?>' class="prof-pic img-thumbnail float-right">
+					<p>Account Created: <?= $account_created ?></p>
 				</div>
 
 				<!-- Patient info-->
 				<div class="col-sm">
-					<div class="row container p-3 border-0">
-						<h1><?= $fname . " " . $lname ?></h1>
-						<h3><?= $healthnum ?></h3>
-						<p>Account Created: <?= $account_created ?></p>
-						<form>
-							<button type="button" id="admin" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-patient">
-								<em class="fa-solid fa-trash-can"></em> Delete this account</button>
-						</form>
-					</div>
+					<form method='post' id="patient-info">
+						<div class="row container p-3 border-0">
+							<div class="form-group row g-1">
+								<div class="form-floating mb-3 mt-3 col">
+									<input type="text" class="form-control form-control-lg" id="fname" placeholder="First Name" name="fname" value="<?= $fname; ?>" required>
+									<label for="fname">First Name</label>
+								</div>
 
-					<div class="row container p-3 border-0">
-						<h4 class="card-title">Contact Info:</h4>
-						<br>
-						<div class="row">
-							<p class="col card-text">Phone Number:</p>
-							<p class="col card-text"><?= $phonenum ?></p>
+								<div class="form-floating mb-3 mt-3 col">
+									<input type="text" class="form-control form-control-lg" id="lname" placeholder="Last Name" name="lname" value="<?= $lname; ?>" required>
+									<label for="lname">Last Name</label>
+								</div>
+							</div>
+
+							<div class="form-floating mb-3 mt-3 col g-2">
+								<input type="text" class="form-control form-control-lg" id="healthnum" placeholder="Healthcard Number" name="healthnum" value="<?= $healthnum; ?>" maxlength="12" required>
+								<label for="lname">Healthcard Number</label>
+							</div>
+							<h3></h3>
+
+							<div class="input-group row g-1">
+								<button type="button" id="delete" class="btn btn-danger col" data-bs-toggle="modal" data-bs-target="#delete-patient">
+									<em class="fa-solid fa-trash-can"></em> Delete this account
+								</button>
+								<button type="submit" id="edit" name="edit" class="btn btn-primary col">
+									<em class="fa-solid fa-floppy-disk"></em> Save this patient profile
+								</button>
+							</div>
 						</div>
 
-						<div class="row">
-							<p class="col card-text">Email Address:</p>
-							<p class="col card-text"><?= $email ?></p>
+						<div class="row container p-3 border-0">
+							<h4 class="card-title">Contact Info:</h4>
+							<br>
+							<div class="row g-2">
+								<div class="form-floating mb-3 mt-3 col">
+									<input type="text" class="form-control form-control-sm" id="phonenum" placeholder="Phone Number" name="phonenum" value="<?= $phonenum; ?>" maxlength="12" required>
+									<label for="lname">Phone Number</label>
+								</div>
+							</div>
+
+							<div class="row g-2">
+								<div class="form-floating mb-3 mt-3 col">
+									<input type="text" class="form-control form-control-sm" id="email" placeholder="Email" name="email" value="<?= $email; ?>" required>
+									<label for="lname">Email Address</label>
+								</div>
+							</div>
+
+							<div class="row g-2">
+								<div class="form-floating mb-3 mt-3 col">
+									<input type="text" class="form-control form-control-sm" id="address" placeholder="Address" name="address" value="<?= $address; ?>" required>
+									<label for="lname">Address</label>
+								</div>
+							</div>
+
+							<div class="row g-2">
+								<div class="form-floating mb-3 mt-3 col">
+									<input type="text" class="form-control form-control-sm" id="postalcode" placeholder="Postal Code" name="postalcode" value="<?= $postalcode; ?>" maxlength="7" required>
+									<label for="lname">Postal Code</label>
+								</div>
+							</div>
+
+							<br>
+							<div class="row g-2">
+								<div class="form-floating mb-3 mt-3 col">
+									<textarea class="form-control" id="biography" placeholder="Biography" name="Biography" value="<?= $biography; ?>" style="height:120px"></textarea>
+									<label for="lname">Biography</label>
+								</div>
+							</div>
 						</div>
-
-						<div class="row">
-							<p class="col card-text">Address:</p>
-							<p class="col card-text"><?= $address ?></p>
-						</div>
-
-						<div class="row">
-							<p class="col card-text">Postal Code:</p>
-							<p class="col card-text"><?= $postalcode ?></p>
-						</div>
-
-						<br>
-						<h4 class="card-title">Biography:</h4>
-
-						<p class="card-text"><?= $biography ?></p>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
